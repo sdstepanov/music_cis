3b3e8fbc8d68c4fdc4a60e17598e101442b9cf27
Sergei Stepanov <94950124+sdstepanov@users.noreply.github.com>
2022-03-13 20:06:35 +0300

    Fix problems

--- select_2.sql
+++ select_2.sql
@@ -12,22 +12,24 @@ select name_album, avg(lenght)
 from composition c join album a on c.id_album = a.id
 group by name_album;
 
-select nickname, year_album
-from musician_album ma
-	join musician m on ma.musician_id  = m.id
-	join album a  on ma.album_id  = a.id
-where year_album not in (2020);
+select distinct nickname
+from musician
+where nickname not in (
+	select distinct nickname
+	from musician m
+		left join musician_album ma on ma.musician_id  = m.id
+		left join album a  on ma.album_id  = a.id
+	where year_album = (2020))
+order by nickname;
 
-select name_collection
+select distinct name_collection
 from musician m 
 	join musician_album ma on ma.musician_id  = m.id
 	join album a on ma.album_id = a.id 
 	join composition c on a.id = c.id_album 
 	join composition_collection cc on c.id = cc.id_composition 
 	join collection c2 on cc.id_collection = c2.id 
-where nickname = 'Eminem'
-group by name_collection;
-
+where nickname = 'Eminem';
 
 select m.id, nickname, count(name_of_genre) 
 	from genre_musician gm 
@@ -35,16 +37,6 @@ select m.id, nickname, count(name_of_genre)
 	join genre g on gm.genre_id = g.id
 group by m.id , nickname
 having count(name_of_genre) > 1;
-
-select name_album 
-from (select m.id, nickname, count(name_of_genre) 
-	from genre_musician gm 
-		join musician m on gm.musician_id = m.id
-		join genre g on gm.genre_id = g.id
-	group by m.id , nickname
-	having count(name_of_genre) > 1) mus
-join musician_album ma on mus.id = ma.musician_id 
-join album a on ma.album_id = a.id;
 
 select name_composition 
 from composition c 
@@ -60,14 +52,18 @@ from musician m
 where lenght in (select min(lenght)
 	from composition);
 
-
-select name_album, cnm
-from (select name_album, count(name_composition) as cnm
-		from album a join composition c on a.id = c.id_album
-		group by name_album) count_composition 
-order by cnm
-limit 1
-
+select distinct name_album 
+from album a join composition c on a.id = c.id_album
+where c.id_album in (
+	select id_album 
+	from composition c2
+	group by id_album
+	having count(c2.id) in (
+		select count(id)
+		from composition
+		group by id_album
+		order by count(id) 
+		limit 1));
 
 
 
